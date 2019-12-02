0e78fedca970dc643a48b70ce44bf3051a92708c
Gregorio21 <GAGlvj@gmail.com>
2019-12-02 17:04:33 -0500

    Interface and search bar
    I added a search form that allows you to search by address using googles api, and select a percentage for distance and elevation. Also it is retractable which is pretty gnarly. Looks like the API key needs to get updated though, as it doesn't have places permissions

--- maps.js
+++ maps.js
@@ -1,11 +1,14 @@
     var map;
+    function displayForm() {
+
+    }
     function initMap() {
     /*
     Maps initialized to UMass Amherst with latlng(42.391155,-72.526711)
     */
     var campus = {lat: 42.3896166, lng: -72.52946829999999};
     map = new google.maps.Map(document.getElementById('map'), {
-        center: campus, 
+        center: campus,
         zoom: 13
     });
     var marker = new google.maps.Marker({position:campus, map:map});
@@ -15,20 +18,26 @@
     var list = document.createElement('ul');
     var item1 = document.createElement('li');
     var item2 = document.createElement('li');
-    item1.textContent = "test message 1";
-    item2.textContent = "test message 2";
+    item1.textContent = "UMass Amherst";
+    //item2.textContent = "test message 2";
     list.appendChild(item1)
-    list.appendChild(item2)
+    //list.appendChild(item2)
     list.classList.add("list");
 
     var pic = document.createElement("img");
     pic.src = "image.jpg";
     pic.classList.add('pics')
 
-    infocontent.appendChild(pic);
+    //infocontent.appendChild(pic);
     infocontent.appendChild(list);
     var infoWindow = new google.maps.InfoWindow;
+    var input1 = document.getElementById('searchStart');
 
+    var searchBox1 = new google.maps.places.SearchBox(input1);
+
+    var input2 = document.getElementById('searchEnd');
+
+    var searchBox2 = new google.maps.places.SearchBox(input2);
     marker.addListener('mouseover',function(){
         infoWindow.setContent(infocontent);
         infoWindow.open(map,marker);
@@ -39,8 +48,7 @@
     });
 
     marker.addListener('click',function(){
-        window.open( "img.html", "_blank"); 
+        window.open( "img.html", "_blank");
     });
 
 }
-
