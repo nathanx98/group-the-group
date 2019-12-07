42e09483f8fdf759b648e7fe277c140f7c312012
nathanx98 <xuanjiyang09@gmail.com>
2019-12-07 16:57:27 -0500

    routing

--- routing_alg.py
+++ routing_alg.py
@@ -19,5 +19,28 @@ G = ox.graph_from_place(place_query, network_type='drive')
 G = ox.add_node_elevations(G, api_key=api_key)
 G = ox.add_edge_grades(G)
 
-def findRoute(graph,start,end, eWeight, dWeight):
+
+G_proj = ox.project_graph(G)
+
+origin = ox.get_nearest_node(G, (42.3868, -72.5301))
+destination = ox.get_nearest_node(G, (42.3418, -72.5884))
+bbox = ox.bbox_from_point((42.3868, -72.5301), distance=6000, project_utm=True)
+
+
+def impedance(length, grade):
+    penalty = grade ** 2
+    return length * penalty
+
+
+for u, v, k, data in G_proj.edges(keys=True, data=True):
+    data['impedance'] = impedance(data['length'], data['grade_abs'])
+    data['rise'] = data['length'] * data['grade']
+
+
+def graph(origin,destination,g):    #pass G_proj as g
+    return nx.shortest_path(g, source=origin, target=destination, weight='impedance')
+
+route_by_impedance = graph(origin,destination,G_proj)
+fig, ax = ox.plot_graph_route(G_proj, route_by_impedance, bbox=bbox, node_size=0)
+
 
