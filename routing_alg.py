2908db623fb543b546e69294d1b9d6225b9558ce
nathanx98 <xuanjiyang09@gmail.com>
2019-12-07 15:49:50 -0500

    Create routing_alg.py

---
+++ routing_alg.py
@@ -0,0 +1,19 @@
+# from keys import AIzaSyCm0ZhqcDDR75EnTs4EfSxFIdFIdowigCs #replace this with your own API key!
+import networkx as nx
+import numpy as np
+import osmnx as ox
+
+api_key = "AIzaSyCm0ZhqcDDR75EnTs4EfSxFIdFIdowigCs"
+
+# place_name = "Amherst,Massachusetts,USA"
+# graph = ox.graph_from_place(place_name)
+# fig,ax = ox.plot_graph(graph)
+
+place = 'Amherst'
+place_query = {'city':'Amherst', 'state':'Massachusetts', 'country':'USA'}
+G = ox.graph_from_place(place_query, network_type='drive')
+
+
+G = ox.add_node_elevations(G, api_key=api_key)
+G = ox.add_edge_grades(G)
+
